d37df45e2ef1624252edc2a8b3d6590034a4ffd8
rodhall85 <rodhall@hotmail.co.uk>
2018-10-10 23:04:11 +0100

    Fix menu not closing on click

--- src/Burger.js
+++ src/Burger.js
@@ -6,15 +6,38 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 
 class Burger extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuIsOpen: false
+    };
+
+    this.menuStateChanged = this.menuStateChanged.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  menuStateChanged(state) {
+    const newState = { menuIsOpen: state.isOpen }
+    this.setState(newState);
+  }
+
+  closeMenu() {
+    this.setState({ menuIsOpen: false });
+  }
+
   render () {
+    const isMenuOpen = state => {
+      return state.isOpen;
+    }
+
     return (
       <Router>
-        <Menu className="menu" right>
-            <Link className="menu-item" to="#home">HOME</Link>
-            <Link className="menu-item" to="#about">ABOUT ME</Link>
-            <Link className="menu-item" to="#treatments">TREATMENTS</Link>
-            <Link className="menu-item" to="#testimonials">TESTIMONIALS</Link>
-            <Link className="menu-item" to="#contact">CONTACT</Link>
+        <Menu className="menu" right onStateChange={ isMenuOpen } isOpen={ this.state.menuIsOpen }>
+            <Link className="menu-item" to="#home" onClick={ this.closeMenu }>HOME</Link>
+            <Link className="menu-item" to="#about" onClick={ this.closeMenu }>ABOUT ME</Link>
+            <Link className="menu-item" to="#treatments" onClick={ this.closeMenu }>TREATMENTS</Link>
+            <Link className="menu-item" to="#testimonials" onClick={ this.closeMenu }>TESTIMONIALS</Link>
+            <Link className="menu-item" to="#contact" onClick={ this.closeMenu }>CONTACT</Link>
         </Menu>
       </Router>
     );
