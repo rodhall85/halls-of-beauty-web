2393cc1a6bb0a435740252583f5083aa273506f2
Rod Hall <rodhall@hotmail.co.uk>
2020-09-19 16:03:13 +0100

    Remove Promotions for now

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         <Header />
         <Intro />
         <About />
-        <Promotions />
+        {/* <Promotions /> */}
         <Treatments />
         <Testimonials />
         <Contact />
