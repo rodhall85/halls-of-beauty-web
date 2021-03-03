3c2fc877fad3f8efd5fbda8ccd2890356f3dd7b7
Rod Hall <rodhall@hotmail.co.uk>
2021-03-03 20:31:17 +0000

    Fix images not loading

--- src/Footer.js
+++ src/Footer.js
@@ -10,7 +10,7 @@ class Footer extends Component {
             <div className="footer-copyright">&copy; {new Date().getFullYear()} Halls of Beauty</div>
             <div className="footer-facebook">
               <a href="https://www.facebook.com/Halls-Of-Beauty-362920180445378">
-                <img src={require("./images/facebook.svg")} alt="facebook icon"></img>
+                <img src={require("./images/facebook.svg").default} alt="facebook icon"></img>
               </a>
             </div>
           </div>
