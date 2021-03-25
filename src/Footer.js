3f4b894028cc30e7c8b89c7f68fbde2c84f32957
Rod Hall <rodhall@hotmail.co.uk>
2021-03-25 22:52:00 +0000

    Amend Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import './Footer.css';
 
-class Footer extends Component {
-  render () {
-    return (
-      <div className="footer-container">
-        <footer>
-          <div className="footer-top">
-            <div className="footer-copyright">&copy; {new Date().getFullYear()} Halls of Beauty</div>
-            <div className="footer-facebook">
-              <a href="https://www.facebook.com/Halls-Of-Beauty-362920180445378">
-                <img src={require("./images/facebook.svg").default} alt="facebook icon"></img>
-              </a>
-            </div>
+const Footer = () => {
+  const bob = 'bill';
+
+  console.log(bob);
+  
+  return (
+    <div className="footer-container">
+      <footer>
+        <div className="footer-top">
+          <div className="footer-copyright">&copy; {new Date().getFullYear()} Halls of Beauty</div>
+          <div className="footer-facebook">
+            <a href="https://www.facebook.com/Halls-Of-Beauty-362920180445378">
+              <img src={require("./images/facebook.svg").default} alt="facebook icon"></img>
+            </a>
           </div>
-          <div className="footer-bottom">
-            <div>
-              <a className="footer-email" href="mailto:bev.hall@outlook.com">bev.hall@outlook.com</a>
-            </div>
-            <div className="footer-phone">07513261203</div>
-          </div>          
-        </footer>
-      </div>
-    );
-  }
+        </div>
+        <div className="footer-bottom">
+          <div>
+            <a className="footer-email" href="mailto:bev.hall@outlook.com">bev.hall@outlook.com</a>
+          </div>
+          <div className="footer-phone">07513261203</div>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default Footer
